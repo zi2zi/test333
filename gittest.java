b3997e7a15a6213ed7066d9e4a09bad0759d1336
zi2zi <59020050+zi2zi@users.noreply.github.com>
2020-01-28 17:48:44 +0900

    Update gittest.java

--- gittest.java
+++ gittest.java
@@ -1,5 +1,33 @@
 class Solution {
     public long solution(int[] classes, int k) {
-      
+        int attempts = 0;
+        try {
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+   
+        } catch (Exception e) {
+            e.printStacktrace();
+        }
+        
+        String str=readString(); // read lengthy string any source db,textbox/jsp etc..
+        // This will place the string in memory pool from which you can't remove
+        str.intern();
+        
+         while(attempts < 5) {
+            try {
+                DescribeTableRequest request = new DescribeTableRequest(tableName);
+                DescribeTableResult result = client.describeTable(request);
+                String status = res.getTable().getTableStatus();
+                if(status.equals("ACTIVE")) {
+                break;
+                }
+                Thread.sleep(5000);
+                attempts++;
+            }
+            catch(ResourceNotFoundException e) {
+                
+                
+                
+            }   
+         }
     }
 }
