ea6fc98ffa2c1e2a9b5efc8c1659b76d0faf02d2
zi2zi <59020050+zi2zi@users.noreply.github.com>
2020-01-28 17:54:36 +0900

    Create gittest2.java

---
+++ gittest2.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Cache {
+      
+       public static void main(String[] args) {            
+              try(GZip gzip = GZipInputStream.create(url.openStream()))
+              {
+                  use(gzip);
+              }
+              catch(Exception e){
+                handle();
+              }
+       }
+}
