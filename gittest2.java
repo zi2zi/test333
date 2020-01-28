53ac221d70e0a7e81fa052ba909db08a0d9a3abf
zi2zi <59020050+zi2zi@users.noreply.github.com>
2020-01-28 18:03:17 +0900

    Update gittest2.java

--- gittest2.java
+++ gittest2.java
@@ -1,14 +1,37 @@
-import java.util.HashMap;
-import java.util.Map;
-public class Cache {
-      
-       public static void main(String[] args) {            
-              try(GZip gzip = GZipInputStream.create(url.openStream()))
-              {
-                  use(gzip);
-              }
-              catch(Exception e){
-                handle();
-              }
-       }
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+
+	private Object[] elements;
+
+	private int size = 0;
+
+	private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+	public Stack() {
+		this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+	}
+
+	public void push(Object e) {
+		this.ensureCapacity();
+		this.elements[size++] = e;
+	}
+
+	public Object pop() {
+		String msg = "123";
+		msg += "456";
+		
+		if (size == 0) {
+			throw new EmptyStackException();
+		}
+
+		return this.elements[--size];
+	}
+
+	private void ensureCapacity() {
+		if (this.elements.length == size) {
+			this.elements = Arrays.copyOf(elements, 2 * size + 1);
+		}
+	}
 }
